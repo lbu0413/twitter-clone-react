3239303047f1a911084656e89663cfd87cccf7de
LBU <vvookly@gmail.com>
2021-05-14 14:30:50 -0400

    basic firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAH6N8AwbgKjyjuyTC7vDVDvKAShapIooo",
+	authDomain: "twitter-clone-7469b.firebaseapp.com",
+	projectId: "twitter-clone-7469b",
+	storageBucket: "twitter-clone-7469b.appspot.com",
+	messagingSenderId: "1018772392293",
+	appId: "1:1018772392293:web:42ea8812a5cdbc69dc398c",
+};
+
+export default firebase.initializeApp(firebaseConfig);
