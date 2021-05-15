4fc8dc68c9b7d0ec29881f8a530aa84073d1c100
LBU <vvookly@gmail.com>
2021-05-15 02:07:36 -0400

    login functionally added with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
 import AppRouter from "./Router";
+import fbase from "../fbase";
+import { authService } from "../fbase";
 
 function App() {
-	return <AppRouter />;
+	const [init, setInit] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	useEffect(() => {
+		authService.onAuthStateChanged((user) => {
+			if (user) {
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setInit(true);
+		});
+	}, []);
+	return (
+		<>
+			{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+			<footer>&copy;{new Date().getFullYear()} Wook</footer>
+		</>
+	);
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
-	return <span>Auth</span>;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [newAccount, setNewAccount] = useState(true);
+	const [error, setError] = useState("");
+
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		if (name === "email") {
+			setEmail(value);
+		} else if (name === "password") {
+			setPassword(value);
+		}
+	};
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			let data;
+			if (newAccount) {
+				data = await authService.createUserWithEmailAndPassword(
+					email,
+					password
+				);
+			} else {
+				data = await authService.signInWithEmailAndPassword(email, password);
+			}
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+
+	const toggleAccount = () => {
+		return setNewAccount((prev) => !prev);
+	};
+
+	return (
+		<div>
+			<form action="" onSubmit={onSubmit}>
+				<input
+					name="email"
+					type="text"
+					placeholder="email"
+					required
+					value={email}
+					onChange={onChange}
+				/>
+				<input
+					name="password"
+					type="password"
+					placeholder="password"
+					required
+					value={password}
+					onChange={onChange}
+				/>
+				<input
+					type="submit"
+					value={newAccount ? "Create account" : "Sign in"}
+				/>
+				{error}
+			</form>
+			<span onClick={toggleAccount}>
+				{newAccount ? "Sign in" : "Create account"}
+			</span>
+			<div>
+				<button>Continue with Google</button>
+				<button>Continue with Github</button>
+			</div>
+		</div>
+	);
 };
 
 export default Auth;
