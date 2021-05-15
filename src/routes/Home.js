f8deadcbe342f0c89019f48a13deb9b84e195cef
LBU <vvookly@gmail.com>
2021-05-15 03:35:56 -0400

    created database with firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
 
 const Home = () => {
-	return <span>Home</span>;
+	const [spit, setSpit] = useState("");
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		await dbService.collection("spits").add({
+			spit,
+			createdAt: Date.now(),
+		});
+		setSpit("");
+	};
+
+	const onChange = (e) => {
+		const { value } = e.target;
+		setSpit(value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					value={spit}
+					onChange={onChange}
+					type="text"
+					placeholder="What's on your mind?"
+					maxLength={120}
+				/>
+				<input type="submit" value="Spit" />
+			</form>
+		</div>
+	);
 };
 
 export default Home;
