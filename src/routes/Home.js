482492500619bd56d72c1056baf1f68bc8e72bc7
LBU <vvookly@gmail.com>
2021-05-16 15:35:30 -0400

    can create and read spits

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "../fbase";
 
 const Home = () => {
 	const [spit, setSpit] = useState("");
+	const [spits, setSpits] = useState([]);
+	const getSpits = async () => {
+		const dbSpits = await dbService.collection("spits").get();
+		dbSpits.forEach((document) => {
+			const spitObject = {
+				...document.data(),
+				id: document.id,
+			};
+			setSpits((prev) => [spitObject, ...prev]);
+		});
+	};
+
+	useEffect(() => {
+		getSpits();
+	}, []);
+
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		await dbService.collection("spits").add({
@@ -16,7 +32,7 @@ const Home = () => {
 		const { value } = e.target;
 		setSpit(value);
 	};
-
+	console.log(spits);
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
@@ -24,11 +40,18 @@ const Home = () => {
 					value={spit}
 					onChange={onChange}
 					type="text"
-					placeholder="What's on your mind?"
+					placeholder="Spit what's on your mind"
 					maxLength={120}
 				/>
 				<input type="submit" value="Spit" />
 			</form>
+			<div>
+				{spits.map((spit) => (
+					<div key={spit.id}>
+						<h4>{spit.spit}</h4>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
