09f85d982abb58831e4ad9159d90c9c8a7a25e37
LBU <vvookly@gmail.com>
2021-05-18 01:40:17 -0400

    image preview functionality added

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import { dbService } from "../fbase";
 const Home = ({ userObj }) => {
 	const [spit, setSpit] = useState("");
 	const [spits, setSpits] = useState([]);
+	const [attachment, setAttachment] = useState();
 	const getSpits = async () => {
 		const dbSpits = await dbService.collection("spits").get();
 		dbSpits.forEach((document) => {
@@ -40,6 +41,18 @@ const Home = ({ userObj }) => {
 		const { value } = e.target;
 		setSpit(value);
 	};
+
+	const onFileChange = (e) => {
+		const { files } = e.target;
+		const file = files[0];
+		const reader = new FileReader();
+		reader.onloadend = (finishedEvent) => {
+			const { result } = finishedEvent.currentTarget;
+			setAttachment(result);
+		};
+		reader.readAsDataURL(file);
+	};
+	const onClearAttachment = () => setAttachment(null);
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
@@ -50,7 +63,14 @@ const Home = ({ userObj }) => {
 					placeholder="Spit what's on your mind"
 					maxLength={120}
 				/>
+				<input type="file" accept="image/*" onChange={onFileChange} />
 				<input type="submit" value="Spit" />
+				{attachment && (
+					<div>
+						<img src={attachment} alt="" width="50px" height="50px" />
+						<button onClick={onClearAttachment}>Clear</button>
+					</div>
+				)}
 			</form>
 			<div>
 				{spits.map((spit) => (
