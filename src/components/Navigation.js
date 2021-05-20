a55dc29f2ae74b363227d99b18b112e73734b0c8
LBU <vvookly@gmail.com>
2021-05-20 16:01:09 -0400

    profile icon added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/fontawesome-free";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = ({ userObj }) => {
 	return (
@@ -15,22 +15,34 @@ const Navigation = ({ userObj }) => {
 					marginTop: 50,
 				}}>
 				<li>
-					<Link to="/" style={{ marginRight: 10 }}>
-						<FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+					<Link
+						to="/"
+						style={{
+							marginRight: 20,
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							justifyContent: "center",
+							fontSize: 12,
+						}}>
+						<FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="3x" />
+
+						<span style={{ marginTop: 10, fontSize: "14px" }}>Home</span>
 					</Link>
 				</li>
 				<li>
 					<Link
 						to="/profile"
 						style={{
-							marginLeft: 10,
+							marginLeft: 20,
 							display: "flex",
 							flexDirection: "column",
 							alignItems: "center",
+							justifyContent: "center",
 							fontSize: 12,
 						}}>
-						<FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />
-						<span style={{ marginTop: 10, fontSize: "24px" }}>
+						<FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="3x" />
+						<span style={{ marginTop: 10, fontSize: "14px" }}>
 							{userObj.displayName
 								? `${userObj.displayName} Profile`
 								: "Profile"}
